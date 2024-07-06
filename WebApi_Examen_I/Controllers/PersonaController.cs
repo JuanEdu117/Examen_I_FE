7d077ff12331c6aed9ffa1d6958302efbda951c9
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-07-05 22:50:50 -0600

    person

---
+++ WebApi_Examen_I/Controllers/PersonaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebApi_Examen_I.Controllers
+{
+    public class PersonaController : Controller
+    {
+        #region EVENTOS DE APERTURA VIEW
+        public async Task<IActionResult> Index()
+        {
+            GestorConexionApis 
+            return View();
+        }
+        #endregion
+    }
+}
