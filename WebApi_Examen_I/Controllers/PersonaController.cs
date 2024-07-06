64f64a6fe4b1a047f6772ce33c8a1f35ad7280dd
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-07-05 23:14:10 -0600

    view list persona

--- WebApi_Examen_I/Controllers/PersonaController.cs
+++ WebApi_Examen_I/Controllers/PersonaController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using WebApi_Examen_I.Models;
 
 namespace WebApi_Examen_I.Controllers
 {
     public class PersonaController : Controller
     {
         #region EVENTOS DE APERTURA VIEW
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index()    //CREAMOS VISTA DE TIPO LIST
         {
-            GestorConexionApis 
-            return View();
+            GestorConexionApis Obj_GECNX = new GestorConexionApis();
+            List<PersonaModel> _lstResultado = await Obj_GECNX.ConsultarPersona();
+            return View(_lstResultado);
         }
+
+
         #endregion
     }
 }
